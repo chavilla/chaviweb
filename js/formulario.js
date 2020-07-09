6ce3b375cf2ce80a405609aa24433362d7120e7a
chavilla <jcharris.villa@gmail.com>
2020-07-08 19:11:57 -0500

    Primer commit

---
+++ js/formulario.js
@@ -0,0 +1,67 @@
+
+export default class Frame {
+    constructor(data) {
+         this.data=data;
+    }
+  
+    send(){    
+  
+      const form=new FormData();
+      form.append('email',this.data.email);
+      form.append('message',this.data.message);
+      form.append('name',this.data.name);
+      form.append('phone',this.data.phone);
+      
+    
+        
+       fetch('send.php',{
+           method:'POST',
+           body:form
+       }).then(res=>{
+         console.log(res); return;
+         
+         return res.json();
+       }).then(data=>{
+         if(data.status===200){
+          this.showMessage(data.response,'notification-exit');
+          this.clean();
+         }
+         else{
+          this.showMessage(data.response,'notification-error');
+          this.clean();
+         }
+       });
+    }
+  
+    showMessage(message,status){
+      const page=document.querySelector('body');
+      const dialog=document.createElement('div');
+      dialog.classList.add('notification',status);
+      dialog.textContent=message;
+  
+  
+      page.insertBefore(dialog, document.querySelector('.whatsapp'));
+      dialog.classList.add('visible');
+  
+      setTimeout(()=>{
+        dialog.classList.add('visible');
+  
+        setTimeout(()=>{
+          dialog.classList.remove('visible');
+          setTimeout(() => {
+            dialog.remove();
+       }, 500);
+        },3000);
+      },100);
+      
+    }
+  
+    clean(){
+      const fields=document.querySelectorAll('input,textarea,select');
+      fields.forEach(field=>{
+        field.value='';
+        field.classList.remove('exit');
+      })
+    }
+  
+  }
---
+++ js/mail.js
@@ -0,0 +1,32 @@
+import Frame from "./formulario.js";
+
+const frm = document.querySelector("#frm");
+const campos = document.querySelectorAll(".campo");
+
+let datos = {};
+
+//Events
+function listener() {
+  frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const frame = new Frame(datos);
+    frame.send();
+  });
+
+  //when the user change the inputs
+  campos.forEach((campo) => {
+    campo.addEventListener("change", onChange);
+  });
+}
+
+//when a input is changed
+function onChange(e) {
+  datos = {
+    ...datos,
+    [e.target.name]: e.target.value,
+  };
+}
+
+//execute functions
+listener();
